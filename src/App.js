cb615432740114c43760963d7d2e1d872a1354e7
torarvill <arvill@kth.se>
2021-10-14 13:02:31 +0200

    basics

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import shark from './sharkgif.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={shark} className="shark" alt="shark" />
+        <p>
+          Yeah, there's not much here.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
